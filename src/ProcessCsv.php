d8dc37360e6bbfa057a37bbe65596f0c359589ed
Dorota <d.a.lewinska@gmail.com>
2021-03-22 15:18:46 +0000

    Function to read csv file adjusted to this project

--- src/ProcessCsv.php
+++ src/ProcessCsv.php
@@ -12,29 +12,16 @@ class ProcessCsv
   {
     $arrReturn = [];
     $file = new \SplFileObject($csv_path, "r");
-    $n = 0;
     while (!$file->eof()) {
-      $n++;
-      if ($n === 1) {
-        $arrColNames = $file->fgetcsv();
-        $count = count($arrColNames);
-      } else {
-        $arrLine = $file->fgetcsv();
- 
-        // check if line not empty
-        $implode = implode('', $arrLine);
-        if (!empty($implode)) {
-          // check if line fields match column names
-          if (count($arrLine) === $count) {
-            foreach ($arrColNames as $k => $colName) {
-              $arr[$colName] = $arrLine[$k];
-            }
-            $arrReturn[] = $arr;
-          }
-        }
+      // this returns line as array
+      $arrLine = $file->fgetcsv();
+      //this turns array to string and removes trailing whitespace
+      $strLine = trim(implode(' ', $arrLine));
+      //this skips empty lines
+      if ($strLine != '') {
+        $arrReturn[] = $strLine;
       }
     }
-
     return $arrReturn;
   }
 }
